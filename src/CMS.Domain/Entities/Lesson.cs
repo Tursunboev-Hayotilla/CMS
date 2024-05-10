2a05483c1933bdd97328cc88b2991cb587c340ff
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-05-10 17:35:03 +0500

    Create Models

---
+++ src/CMS.Domain/Entities/Lesson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Domain.Entities
+{
+    public class Lesson
+    {
+        public Guid Id { get; set; } = new Guid();
+        public required string Theme { get; set; }
+        public object subjectId { get; set; }
+        public required Day? day { get; set; }
+        public required TimeOnly? fromTime {  get; set; }
+        public required TimeOnly? toTime {  get; set; }
+        public object classId { get; set; } 
+
+    }
+}
