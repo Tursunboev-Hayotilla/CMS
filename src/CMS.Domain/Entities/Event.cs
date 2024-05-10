e1433091584919a9bb69520ab913e3ba8e0e73c8
tohirjon-odilov <shahanshoh819@gmail.com>
2024-05-10 17:35:59 +0500

    Add model school gender location school student event

---
+++ src/CMS.Domain/Entities/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Domain.Entities
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required DateTimeOffset Date { get; set; }
+        public object SchoolId { get; set; }
+        public string? Description { get; set; }
+        public string? PhotoPath { get; set; }
+    }
+}
